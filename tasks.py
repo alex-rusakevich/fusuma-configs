102af464cfe10b6a3455de1ef94ae307bdb1f12c
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2023-10-20 00:57:17 +0300

    #patch added remove fusuma-config to tasks.py

--- tasks.py
+++ tasks.py
@@ -23,8 +23,27 @@ def install(context):
         print(
             "'~/.config/fusuma/config.yml' already exists, backing it up as '~/.config/fusuma/config.yml.bak'..."
         )
-        run("cp ~/.config/fusuma/config.yml ~/.config/fusuma/config.yml.bak")
+        run("mv ~/.config/fusuma/config.yml ~/.config/fusuma/config.yml.bak")
 
     run(f"cp '{os.path.join('configs', chosen_opt)}' ~/.config/fusuma/config.yml")
+    print("The config has been installed successfully! Restart fusuma, please")
 
-    print("The config has been installed successfully!")
+
+@task
+def remove(context):
+    should_continue = (
+        input(
+            "Do you really want to remove fusuma config? \
+All the changes in fusuma/config.yml will be lost! [y/n] "
+        )
+        .strip()
+        .lower()
+        == "y"
+    )
+
+    if not should_continue:
+        print("Bye!")
+        return
+
+    run("rm ~/.config/fusuma/config.yml")
+    print("The config has been removed successfully! Restart fusuma, please")
